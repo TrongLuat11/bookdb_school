c0ad099fbce782ec6bf28a185c1c7e500b7f56bc
030239230132-star <030239230132@st.buh.edu.vn>
2026-03-31 08:50:54 +0700

    Thêm chức năng Quản lý danh mục Thể loại

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\LayoutController;
+use App\Http\Controllers\QuanLyTheLoaiController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -21,6 +22,9 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // Quản lý thể loại (Đã đưa trở lại vào trong Auth để bảo mật khi merge code)
+    Route::resource('quan-ly-the-loai', QuanLyTheLoaiController::class);
 });
 
 require __DIR__.'/auth.php';
